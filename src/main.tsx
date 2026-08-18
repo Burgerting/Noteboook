b6fe52d2ef5fcb2daacc048eef272539dc7fe6f0
Noteboook User <anonymous@example.com>
2026-08-18 08:43:07 +0800

    fix: hardcode Google Client ID for GitHub Actions build

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 import { AuthProvider } from './store/AuthContext.tsx'
 import './index.css'
 
-const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '255469969680-ml7tkfnvm9acks5eto81c96i553n450b.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
